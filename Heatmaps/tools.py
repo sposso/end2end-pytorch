1f298efdfc198609ce227887d7d533ad57d358dc
Santiago Posso <97705324+sposso@users.noreply.github.com>
2023-01-21 14:54:02 -0500

    Update tools.py

--- Heatmaps/tools.py
+++ Heatmaps/tools.py
@@ -31,3 +31,32 @@ def sweep_img_patches(img, patch_size, stride):
             patch = img[y:y+patch_size, x:x+patch_size].copy()
             patch_list.append(patch.astype('float32'))
     return np.stack(patch_list), nb_row, nb_col
+
+
+def initialize_model(model_name, num_classes,use_pretrained):
+    '''The architecture employed for the patch classifier is the 
+    resnet50. Since this patch model classifies 5 patch categories, it is 
+    necessary to adjust the default  resnet50 of pytorch, which classifies 
+    1000 classes'''
+    model_ft = None
+    
+
+    if model_name == "resnet":
+        """ Resnet50
+        """
+        model_ft = models.resnet50()
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        if use_pretrained =='s':
+            #Load the trained patch classifier's parameters on the s dataset. 
+            model_ft.load_state_dict(torch.load('.../best_s_patch_model.pt',map_location=torch.device('cpu')))
+            
+            print('Initialzing model with s Patch Classifier Weights')
+            #Load the trained patch classifier's parameters on the s10 dataset.
+        elif use_pretrained == 's10':
+            model_ft.load_state_dict(torch.load('.../best_s10_patch_model.pt',map_location=torch.device('cpu')))
+            print('Initialzing model with s10 Patch Classifier Weights')
+            
+            
+        
+    return model_ft
